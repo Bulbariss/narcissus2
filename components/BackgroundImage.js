029ea21c35cc29e061d69c1a2af824e2fa05edd1
Vladimir Radionovsky <dudefromyalta@gmail.com>
2021-01-02 14:55:52 +0300

    added favicon

--- components/BackgroundImage.js
+++ components/BackgroundImage.js
@@ -7,31 +7,20 @@ export default function BackgroundImage({ image, children }) {
     <div className="relative flex items-center justify-center w-screen min-h-screen">
       <div className="absolute w-full h-full">
         <div className="relative w-full h-full overflow-hidden">
-          {/* Our placeholder image */}
           <img
             aria-hidden="true"
             alt=""
-            className="transition-all duration-200 select-none -z"
+            className="absolute top-0 bottom-0 left-0 right-0 object-cover w-full h-full transition-all duration-200 select-none -z"
             src={`/_next/image?url=${image}&w=80&q=75`}
+            loading="eager"
             style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              zIndex: -1,
-              right: 0,
-              bottom: 0,
-              width: "100%",
-              height: "100%",
-              /* Adjust the content to fit */
-              objectFit: "cover",
               objectPosition: "center",
-              /* Blur the image and scale to avoid transparent corners */
               filter: "blur(2rem)",
               transform: "scale(1.2)",
             }}
           />
-          {/* Your image, optimized by next/image */}
           <Image
+            loading="eager"
             onLoad={() => setLoaded(true)}
             layout="fill"
             quality={75}
@@ -45,15 +34,6 @@ export default function BackgroundImage({ image, children }) {
         </div>
       </div>
       {children}
-      <style jsx global>{`
-        .blur-up {
-          transition-duration: 800ms;
-          opacity: 0;
-        }
-        .blur-up.loaded {
-          opacity: 0.99;
-        }
-      `}</style>
     </div>
   );
 }
