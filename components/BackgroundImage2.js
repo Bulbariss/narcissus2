1407d8574824cb0ea3594d11fe759d3ca9748410
Vladimir Radionovsky <dudefromyalta@gmail.com>
2021-01-03 12:35:58 +0300

    bug fix

--- components/BackgroundImage2.js
+++ components/BackgroundImage2.js
@@ -4,37 +4,36 @@ import Head from "next/head";
 
 export default function BackgroundImage({ image, children }) {
   let imageRef = useRef();
-  function anim() {
-    let imageElement2 = imageRef.current.getElementsByClassName("bg-image")[0];
-    imageElement2.classList.add("loaded");
+  const placeholderUrl = `/_next/image?url=${image}&w=40&q=75`;
+
+  function imageLoaded() {
+    let imagePlaceholder = imageRef.current.getElementsByClassName(
+      "bg-image"
+    )[0];
+    imagePlaceholder.classList.add("loaded");
   }
-  console.log("object");
   return (
     <>
       <Head>
-        <link
-          rel="preload"
-          href={`/_next/image?url=${image}&w=40&q=75`}
-          as="image"
-        />
+        <link rel="preload" href={placeholderUrl} as="image" />
       </Head>
       <div
         ref={imageRef}
         className="relative flex flex-col items-center justify-center w-screen min-h-screen"
       >
         <Image
-          onLoad={() => anim()}
+          onLoad={() => imageLoaded()}
           layout="fill"
           quality={75}
           objectFit="cover"
           src={image}
           alt=""
           className="select-none test123 -z"
-        ></Image>
+        />
         <div className="absolute top-0 left-0 w-full h-full">
           <div
             style={{
-              backgroundImage: `url("/_next/image?url=${image}&w=80&q=75")`,
+              backgroundImage: `url("${placeholderUrl}")`,
             }}
             className={`bg-image w-full h-full z-10 delay-200`}
           ></div>
