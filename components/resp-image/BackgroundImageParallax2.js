3a474b7754a17e18b2d88a1b254eefa6d6d599c7
Vladimir Radionovsky <dudefromyalta@gmail.com>
2021-01-31 15:24:07 +0300

    better parallax

--- components/resp-image/BackgroundImageParallax2.js
+++ components/resp-image/BackgroundImageParallax2.js
@@ -1,33 +1,18 @@
 const Parallax = ({ image }) => {
   return (
-    <div className="relative w-screen h-screen">
-      <div className="absolute bbb">
-        <div className="bbb2">
-          <div className="ttt">
-            <div className="ttt2">
-              <div className="nnn">
-                <div
-                  className="nnn2"
-                  style={{ backgroundImage: `url(${image})` }}
-                ></div>
-              </div>
-            </div>
+    <div className="relative w-screen h-screen -z">
+      <div className="ttt">
+        <div className="ttt2">
+          <div className="nnn">
+            <div
+              className="nnn2"
+              style={{ backgroundImage: `url(${image})` }}
+            ></div>
           </div>
         </div>
       </div>
       <style jsx>
         {`
-          .bbb2 {
-            height: 100%;
-            background: #f6f5f4;
-            position: initial;
-          }
-          .bbb {
-            height: 100vh;
-            background: #f6f5f4;
-            position: relative;
-            z-index: -1;
-          }
           .ttt {
             margin-top: -100vh;
             transform: scaleY(0.25);
@@ -45,7 +30,6 @@ const Parallax = ({ image }) => {
           .nnn {
             width: 100vw;
             height: 400vh;
-            position: fixed;
             top: 0;
           }
 
@@ -72,6 +56,11 @@ const Parallax = ({ image }) => {
 
               .nnn {
                 height: 200%;
+                position: fixed;
+                will-change: transform;
+                perspective: 1000;
+                transform: translate3d(0);
+                backface-visibility: hidden;
               }
 
               .nnn2 {
