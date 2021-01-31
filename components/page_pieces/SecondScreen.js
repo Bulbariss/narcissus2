39cad3aaa2fda49972a16f106efb7839993f05ad
Vladimir Radionovsky <dudefromyalta@gmail.com>
2021-01-31 13:54:23 +0300

    better hero

--- components/page_pieces/SecondScreen.js
+++ components/page_pieces/SecondScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 const SecondScreen = () => {
   return (
     <section className="relative px-4 py-32 bg-gray-900 md:text-center color-white">
-      <h2 className="pb-6 H1 bbb">О чем наш проект</h2>
+      <h2 className="pb-4 H1 bbb">О чем наш проект</h2>
       <p className="mx-auto text-gray-300 whitespace-pre-line max-w-text P">
         {
           "Проект состоит из атмосферного mood-видео, серии фото-коллажей и исследования, в рамках которого команда углубилась в проблему вместе с психологом и получила советы, которые помогут распознать\nнарцисса-абьюзера."
