8bc622b4256e1bc0b873b72e20e817334bcefb53
Vladimir Radionovsky <dudefromyalta@gmail.com>
2021-02-10 16:41:17 +0300

    update

--- components/page_pieces/Hero.js
+++ components/page_pieces/Hero.js
@@ -5,61 +5,59 @@ import { useMedia } from "react-use";
 
 const Hero = ({ Test2, Test, data }) => {
   const isLandscape = useMedia("(orientation: landscape)");
-  const pageContent = (
-    <div className="relative flex flex-col items-center justify-between w-full h-full min-h-screen">
-      <div
-        className="absolute w-screen h-full min-h-screen bg-black"
-        style={{ opacity: "0.4" }}
-      />
-      <div
-        className="absolute bottom-0 w-screen h-48 pt-16"
-        style={{ background: "linear-gradient( transparent, #212121)" }}
-      />
-      <div
-        className="absolute top-0 w-screen h-48 pb-16"
-        style={{ background: "linear-gradient(#000, transparent)" }}
-      ></div>
-      <div className="top-0 z-10 w-screen h-auto">
-        <p className="py-6 text-4xl text-center bbb">{data.smi}</p>
-        <div className="flex justify-around max-w-80" style={{ color: "#cf0" }}>
-          {data.linksToPublications.map((item) => (
-            <a
-              key={item.title}
-              rel="noreferrer"
-              target="_blank"
-              href={item.link}
-            >
-              {item.title}
-            </a>
-          ))}
-        </div>
-      </div>
-      <div className="z-10 flex flex-col max-w-2xl px-4 text-white md:px-0 hero-main">
-        <h1 className="invisible">Narcissus</h1>
-        <img
-          className="w-full h-auto select-none"
-          src={data.narcissusLogo}
-          alt="Narcissus"
-        />
-
-        <p className="text-xl text-white P font-base md:text-center">
-          {data.heroText}
-        </p>
-      </div>
-      <div className=""></div>
-    </div>
-  );
   return (
     <>
-      {isLandscape ? (
-        <BackgroundImage className="" alt="Обложка" image={Test2}>
-          {pageContent}
-        </BackgroundImage>
-      ) : (
-        <BackgroundImage className="" alt="Обложка" image={Test}>
-          {pageContent}
-        </BackgroundImage>
-      )}
+      <BackgroundImage
+        className=""
+        alt="Обложка"
+        image={!isLandscape ? Test : Test2}
+      >
+        <div className="relative flex flex-col items-center justify-between w-full h-full min-h-screen">
+          <div
+            className="absolute w-screen h-full min-h-screen bg-black"
+            style={{ opacity: "0.4" }}
+          />
+          <div
+            className="absolute bottom-0 w-screen h-48 pt-16"
+            style={{ background: "linear-gradient( transparent, #212121)" }}
+          />
+          <div
+            className="absolute top-0 w-screen h-48 pb-16"
+            style={{ background: "linear-gradient(#000, transparent)" }}
+          ></div>
+          <div className="top-0 z-10 w-screen h-auto">
+            <p className="py-6 text-4xl text-center bbb">{data.smi}</p>
+            <div
+              className="flex justify-around max-w-80"
+              style={{ color: "#cf0" }}
+            >
+              {data.linksToPublications.map((item) => (
+                <a
+                  key={item.title}
+                  rel="noreferrer"
+                  target="_blank"
+                  href={item.link}
+                >
+                  {item.title}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="z-10 flex flex-col max-w-2xl px-4 text-white md:px-0 hero-main">
+            <h1 className="invisible">Narcissus</h1>
+            <img
+              className="w-full h-auto select-none"
+              src={data.narcissusLogo}
+              alt="Narcissus"
+            />
+
+            <p className="text-xl text-white P font-base md:text-center">
+              {data.heroText}
+            </p>
+          </div>
+          <div className=""></div>
+        </div>
+      </BackgroundImage>
     </>
   );
 };
