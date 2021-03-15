22e9eb51da814843b771c1274087a333ecdf9576
agencyattolini <mail@contentpanel.xyz>
2021-03-15 18:10:13 +0300

    update

--- components/resp-image/BackgroundImageParallax3.js
+++ components/resp-image/BackgroundImageParallax3.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useRef } from "react";
 import useIntersect from "../utils/useIntersect";
 import { calculateVerticalPercentage } from "../utils/getViewportPercentage";
+
 const ParallaxImage = ({ image }) => {
   const [ref, entry] = useIntersect({
     threshold: 0,
   });
   let el = useRef();
-
   let waiting = false;
   function getPercent() {
     if (!waiting) {
-      el.current.style.transform = `translateY(${
-        (calculateVerticalPercentage(el.current) - 0.5) * 100
-      }%)`;
+      el.current.style.transform = `translateY(${(
+        (calculateVerticalPercentage(el.current) - 0.5) *
+        100
+      ).toFixed(2)}%)`;
 
       waiting = true;
       setTimeout(function () {
@@ -34,24 +35,19 @@ const ParallaxImage = ({ image }) => {
   }, [entry]);
 
   return (
-    <div
-      ref={ref}
-      className="relative w-full min-h-screen overflow-x-visible space-holder"
-    >
-      <div className="sticky top-0 w-screen h-screen overflow-hidden stickyContainer">
-        <div ref={el} className="horizontal"></div>
-      </div>
+    <div ref={ref} className="relative w-screen h-screen overflow-hidden">
+      <div
+        ref={el}
+        className="absolute top-0 w-screen bg-center bg-cover vertical"
+      ></div>
       <style jsx>{`
-        .horizontal {
+        .vertical {
           background-image: url(${image});
-          background-size: 100vmax 125vmax;
-          background-position: center;
           will-change: transform;
+          transform: translate3d(0);
+          backface-visibility: hidden;
           margin-top: -50vh;
           height: 200vh;
-          width: 100vw;
-          position: absolute;
-          top: 0;
         }
       `}</style>
     </div>
