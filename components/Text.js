1e0b9f80446d943d1e8b4270f0127a0deda5b58c
agencyattolini <mail@contentpanel.xyz>
2021-03-29 12:56:36 +0300

    update

--- components/Text.js
+++ components/Text.js
@@ -2,9 +2,6 @@ import React from "react";
 import BackgroundImage from "../components/resp-image/BackgroundImage";
 
 const TextBlock = ({ content, image }) => {
-  var re = /абьюз/gi;
-
-  var newstr = content.replace(re, "апельсины");
   return (
     <BackgroundImage
       className="w-full px-4 py-16 bg-cover md:px-0"
@@ -16,7 +13,7 @@ const TextBlock = ({ content, image }) => {
     >
       <div
         className="max-w-3xl mx-auto leading-7 prose prose-lg text-justify text-white"
-        dangerouslySetInnerHTML={{ __html: newstr }}
+        dangerouslySetInnerHTML={{ __html: content }}
       />
     </BackgroundImage>
   );
